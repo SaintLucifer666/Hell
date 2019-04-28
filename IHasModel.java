6dff233f0a18570ed1facced583caf3acdf80db4
SaintLucifer666 <50069381+SaintLucifer666@users.noreply.github.com>
2019-04-28 15:42:13 +0800

    Add files via upload

---
+++ IHasModel.java
@@ -0,0 +1,8 @@
+package com.saintlucifer.medievalmod.util;
+
+public interface IHasModel 
+{
+	
+	public void registerModels();
+
+}
---
+++ ModItems.java
@@ -0,0 +1,18 @@
+package com.saintlucifer.medievalmod.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.saintlucifer.medievalmod.items.ItemBase;
+
+import net.minecraft.item.Item;
+
+public class ModItems 
+{
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item COPPER = new ItemBase("copper");
+	public static final Item COPPER_INGOT = new ItemBase("copper_ingot");	 
+	
+	
+}
